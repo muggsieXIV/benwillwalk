4db528260b74f8c17ec9713f1fd1b0331d4334a6
muggsieXIV <55777246+muggsieXIV@users.noreply.github.com>
2021-01-16 22:00:40 -0700

    Update settings.py
    Removed Secret Key

--- benwillwalk/settings.py
+++ benwillwalk/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ap-^a$vfnck2u(4cnlm7o_nie4lx5k&lx!$38ia6b)lq6*rsp0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
